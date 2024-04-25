480869757b7eabb2ac87c30b1a186155c0902a58
samian2002 <samiantariq14@gmail.com>
2024-04-25 02:56:16 +0200

    last commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 // Prendo le informazioni e calcolo
 function calcolaPrezzo() {
     const nomeCognome = document.getElementById("nome-cognome").value;
-    const kmDaPercorrere = document.getElementById("km-da-percorrere").value;
-    const eta = document.getElementById("eta");
+    const kmDaPercorrere = parseInt(document.getElementById("km-da-percorrere").value);
+    const eta = parseInt(document.getElementById("eta").value);
     // Metto numero di carrozze disponibili
-    const carrozze = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
+    const carrozze = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     //Genero un numero casuale a 5 cifre per il codice CP
-    const codiceCP = Math.floor(Math.random() + 90000) + 10000;
+    const codiceCP = Math.floor(Math.random() * 90000) + 10000;
     //Costo
     const costoBase = 0.21;
     let costoBiglietto = costoBase * kmDaPercorrere;
@@ -15,8 +15,8 @@ function calcolaPrezzo() {
         costoBiglietto *= 0.8;
     }
     //Sconto 40%
-    else (eta > 65) {
-        costoBiglietto *= 0.6
+    else if (eta > 65) {
+        costoBiglietto *= 0.6;
     }
     //Seleziono a caso la carrozza
     const carrozza = carrozze[Math.floor(Math.random() * carrozze.length)];
@@ -29,4 +29,11 @@ function calcolaPrezzo() {
     document.getElementById("dettagli-biglietto").style.display = "block";
 
 
+    function annulla() {
+        // Resetta i valori dei campi input
+        document.getElementById("nome-cognome").value = "";
+        document.getElementById("km-da-percorrere").value = "";
+        document.getElementById("eta").value = "";
+    }
+4
 }
